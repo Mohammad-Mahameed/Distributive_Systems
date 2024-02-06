b0245b77c0209b488da3c13bdeaeea7fa88535d0
Muhammad Mahameed <mahameedm1999@gmail.com>
2024-02-06 19:05:59 +0200

    refactor

--- demo/src/main/java/com/example/LocalApp.java
+++ demo/src/main/java/com/example/LocalApp.java
@@ -14,16 +14,20 @@ import software.amazon.awssdk.services.ec2.model.InstanceType;
 import software.amazon.awssdk.services.ec2.model.RunInstancesRequest;
 import software.amazon.awssdk.services.ec2.model.Tag;
 import software.amazon.awssdk.services.ec2.model.TerminateInstancesRequest;
+
 public class LocalApp {
 
     final static AWS aws = AWS.getInstance();
+
     public static void main(String[] args) {// args = [inFilePath, outFilePath, tasksPerWorker, -t (terminate, optional)]
         String ec2Script = "#!/bin/bash\n" +
                             "echo Hello World\n";
+
         aws.createManagerIfNotExists(ec2Script);
-        DescribeInstancesRequest request = DescribeInstancesRequest.builder().nextToken(null)
-        .build();
+
+        DescribeInstancesRequest request = DescribeInstancesRequest.builder().nextToken(null).build();
         System.out.println("request " + request.toString());
+
         DescribeInstancesResponse response = aws.ec2.describeInstances(request);
         System.out.println("reservations size " + response.reservations().size());
         System.out.println("response " + response.toString());
