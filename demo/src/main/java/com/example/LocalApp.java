0684d8abe0b085bc1e15e21d7a7e889df391e72e
Muhammad Mahameed <mahameedm1999@gmail.com>
2024-02-06 19:40:03 +0200

    Parsed args

--- demo/src/main/java/com/example/LocalApp.java
+++ demo/src/main/java/com/example/LocalApp.java
@@ -1,7 +1,14 @@
 package com.example;
+import java.util.concurrent.atomic.*;
+
 import software.amazon.awssdk.services.ec2.model.DescribeInstancesRequest;
 import software.amazon.awssdk.services.ec2.model.DescribeInstancesResponse;
 import software.amazon.awssdk.services.ec2.model.Reservation;
+
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -20,6 +27,12 @@ public class LocalApp {
     final static AWS aws = AWS.getInstance();
 
     public static void main(String[] args) {// args = [inFilePath, outFilePath, tasksPerWorker, -t (terminate, optional)]
+        //Prase args
+        LinkedList<String> inputFilesPaths = new LinkedList<String>() , outputFilesPaths = new LinkedList<String>();
+        AtomicInteger n = new AtomicInteger(0);
+        AtomicBoolean terminate = new AtomicBoolean(false);
+        parseArgs(args, inputFilesPaths, outputFilesPaths, n, terminate);
+
         String ec2Script = "#!/bin/bash\n" +
                             "echo Hello World\n";
 
@@ -31,5 +44,25 @@ public class LocalApp {
         DescribeInstancesResponse response = aws.ec2.describeInstances(request);
         System.out.println("reservations size " + response.reservations().size());
         System.out.println("response " + response.toString());
+    }
+
+    
+    public static void parseArgs(String[] args, LinkedList<String> inputFilesPaths, LinkedList<String> outputFilesPaths, AtomicInteger n, AtomicBoolean terminate){
+        int argsSize = args.length, inputOutputFilesCount;
+
+        if(argsSize % 2 == 0){   //terminate is defined
+            terminate.set(true);
+            inputOutputFilesCount = (argsSize-2)/2;
+            n.set(Integer.valueOf(args[argsSize-2]));
+        }
+        else{
+            inputOutputFilesCount = (argsSize-1)/2;
+            n.set(Integer.valueOf(args[argsSize-1]));
+        }
+
+        for(int i=0; i<inputOutputFilesCount; i++){
+            inputFilesPaths.add(args[i]);
+            outputFilesPaths.add(args[inputOutputFilesCount + i]);
+        }
     }
 }
